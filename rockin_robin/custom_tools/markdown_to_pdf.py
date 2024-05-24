ea8369c3f1bef0c2edd59ed86469f5ced6d8d94e
Joshua Oliphant <joshua.oliphant@hey.com>
2024-05-24 10:54:17 -0700

    - Added Poetry for dependency management - Changed markdown to pdf conversion from a tool used by an agency to just calling directly. - If the pdf file exists, I create a new file with a number appended to the file name.

--- rockin_robin/custom_tools/markdown_to_pdf.py
+++ rockin_robin/custom_tools/markdown_to_pdf.py
@@ -1,15 +1,23 @@
 from crewai_tools import BaseTool
 import pypandoc
+import os
 
-    
+
 class MarkdownToPDFTool(BaseTool):
     name: str = "MarkdownToPDFTool"
     description: str = "Converts markdown files to PDF using pypandoc."
     markdown_file_path: str = None
 
     def _run(self) -> str:
-        # Define the output PDF file path
-        pdf_file_path = self.markdown_file_path.replace('.md', '.pdf')
+        # Define the base output PDF file path
+        base_pdf_file_path = self.markdown_file_path.replace('.md', '.pdf')
+        pdf_file_path = base_pdf_file_path
+
+        # Check if the file exists and modify the path to make it unique
+        counter = 1
+        while os.path.exists(pdf_file_path):
+            pdf_file_path = base_pdf_file_path.replace('.pdf', f'_{counter}.pdf')
+            counter += 1
 
         # Convert markdown file to PDF and save it
         pypandoc.convert_file(
--- rockin_robin/main.py
+++ rockin_robin/main.py
@@ -6,10 +6,8 @@ from crewai_tools import (
   SerperDevTool
 )
 from custom_tools.markdown_to_pdf import MarkdownToPDFTool
-import shutil
 import click
 from trogon import tui
-import os
 
 search_tool = SerperDevTool()
 scrape_tool = ScrapeWebsiteTool()
@@ -22,90 +20,81 @@ read_tailored_resume = FileReadTool(file_path='tailored_resume.md')
 # Agent 1: Researcher
 researcher = Agent(
     role="Tech Job Researcher",
-    goal="Make sure to do amazing analysis on "
-         "job posting to help job applicants",
+    goal="""Make sure to do amazing analysis on
+         job posting to help job applicants""",
     tools=[scrape_tool, search_tool],
     verbose=True,
     backstory=(
-        "As a Job Researcher, your prowess in "
-        "navigating and extracting critical "
-        "information from job postings is unmatched."
-        "Your skills help pinpoint the necessary "
-        "qualifications and skills sought "
-        "by employers, forming the foundation for "
-        "effective application tailoring."
+        """
+        As a Job Researcher, your prowess in
+        navigating and extracting critical
+        information from job postings is unmatched.
+        Your skills help pinpoint the necessary
+        qualifications and skills sought
+        by employers, forming the foundation for
+        effective application tailoring.
+        """
     )
 )
 
 # Agent 2: Profiler
 profiler = Agent(
     role="Personal Profiler for Engineers",
-    goal="Do increditble research on job applicants "
-         "to help them stand out in the job market",
+    goal="""Do increditble research on job applicants
+         to help them stand out in the job market""",
     tools=[scrape_tool, search_tool, read_resume, semantic_search_resume],
     verbose=True,
     backstory=(
-        "Equipped with analytical prowess, you dissect "
-        "and synthesize information "
-        "from diverse sources to craft comprehensive "
-        "personal and professional profiles, laying the "
-        "groundwork for personalized resume enhancements."
+        """Equipped with analytical prowess, you dissect
+        and synthesize information
+        from diverse sources to craft comprehensive
+        personal and professional profiles, laying the
+        groundwork for personalized resume enhancements."""
     )
 )
 
 # Agent 3: Resume Strategist
 resume_strategist = Agent(
     role="Resume Strategist for Engineers",
-    goal="Find all the best ways to make a "
-         "resume stand out in the job market.",
+    goal="""Find all the best ways to make a
+         resume stand out in the job market.""",
     tools=[scrape_tool, search_tool, read_resume, semantic_search_resume],
     verbose=True,
     backstory=(
-        "With a strategic mind and an eye for detail, you "
-        "excel at refining resumes to highlight the most "
-        "relevant skills and experiences, ensuring they "
-        "resonate perfectly with the job's requirements."
+        """With a strategic mind and an eye for detail, you
+        excel at refining resumes to highlight the most
+        relevant skills and experiences, ensuring they
+        resonate perfectly with the job's requirements."""
     )
 )
 
 # Agent 4: Interview Preparer
 interview_preparer = Agent(
     role="Engineering Interview Preparer",
-    goal="Create interview questions and talking points "
-         "based on the resume and job requirements",
+    goal="""Create interview questions and talking points
+         based on the resume and job requirements""",
     tools=[scrape_tool, search_tool, read_resume, semantic_search_resume],
     verbose=True,
     backstory=(
-        "Your role is crucial in anticipating the dynamics of "
-        "interviews. With your ability to formulate key questions "
-        "and talking points, you prepare candidates for success, "
-        "ensuring they can confidently address all aspects of the "
-        "job they are applying for."
-    )
-)
-
-# Agent 5: Markdown to PDF Converter
-markdown_to_pdf_converter = Agent(
-    role="Markdown to PDF Converter",
-    goal="Take a markdown file and convert it to a PDF file",
-    tools=[read_tailored_resume, markdown_to_pdf_converter_tool],
-    verbose=True,
-    backstory=(
-        """Your role is to convert the tailored resume from markdown format to PDF format."""
+        """Your role is crucial in anticipating the dynamics of
+        interviews. With your ability to formulate key questions
+        and talking points, you prepare candidates for success,
+        ensuring they can confidently address all aspects of the
+        job they are applying for."""
     )
 )
 
 # Task for Researcher Agent: Extract Job Requirements
 research_task = Task(
     description=(
-        "Analyze the job posting URL provided ({job_posting_url}) "
-        "to extract key skills, experiences, and qualifications "
-        "required. Use the tools to gather content and identify "
-        "and categorize the requirements."
+        """Analyze the job posting URL provided ({job_posting_url})
+        to extract key skills, experiences, and qualifications
+        required. Use the tools to gather content and identify
+        and categorize the requirements."""
     ),
     expected_output=(
-        "A structured list of job requirements, including necessary "
-        "skills, qualifications, and experiences."
+        """A structured list of job requirements, including necessary
+        skills, qualifications, and experiences."""
     ),
     agent=researcher,
     async_execution=True
@@ -114,15 +103,15 @@ research_task = Task(
 # Task for Profiler Agent: Compile Comprehensive Profile
 profile_task = Task(
     description=(
-        "Compile a detailed personal and professional profile "
-        "using the GitHub ({github_url}) URLs, and personal write-up "
-        "({personal_writeup}). Utilize tools to extract and "
-        "synthesize information from these sources."
+        """Compile a detailed personal and professional profile
+        using the GitHub ({github_url}) URLs, and personal write-up
+        ({personal_writeup}). Utilize tools to extract and
+        synthesize information from these sources."""
     ),
     expected_output=(
-        "A comprehensive profile document that includes skills, "
-        "project experiences, contributions, interests, and "
-        "communication style."
+        """A comprehensive profile document that includes skills,
+        project experiences, contributions, interests, and
+        communication style."""
     ),
     agent=profiler,
     async_execution=True
@@ -131,21 +120,21 @@ profile_task = Task(
 # Task for Resume Strategist Agent: Align Resume with Job Requirements
 resume_strategy_task = Task(
     description=(
-        "Using the profile and job requirements obtained from "
-        "previous tasks, tailor the resume to highlight the most "
-        "relevant areas. Employ tools to adjust and enhance the "
-        "resume content. Make sure this is the best resume even but "
-        "don't make up any information. Update every section, "
-        "inlcuding the initial summary, work experience, skills, "
-        "and education. All to better reflrect the candidates "
-        "abilities and how it matches the job posting. The final "
-        "output should be a complete resume. Any changes to style, "
-        "font, spacing, emphasis, etc., should be reflected throughout the document."
+        """Using the profile and job requirements obtained from
+        previous tasks, tailor the resume to highlight the most
+        relevant areas. Employ tools to adjust and enhance the
+        resume content. Make sure this is the best resume even but
+        don't make up any information. Update every section,
+        inlcuding the initial summary, work experience, skills,
+        and education. All to better reflrect the candidates
+        abilities and how it matches the job posting. The final
+        output should be a complete resume. Any changes to style,
+        font, spacing, emphasis, etc., should be reflected throughout the document."""
     ),
     expected_output=(
-        "An updated resume that effectively highlights the candidate's "
-        "qualifications and experiences relevant to the job. Only the "
-        "resume should be outputted, not any part of the conversation or other information."
+        """An updated resume that effectively highlights the candidate's
+        qualifications and experiences relevant to the job. Only the
+        resume should be outputted, not any part of the conversation or other information."""
     ),
     output_file="tailored_resume.md",
     context=[research_task, profile_task],
@@ -155,50 +144,36 @@ resume_strategy_task = Task(
 # Task for Interview Preparer Agent: Develop Interview Materials
 interview_preparation_task = Task(
     description=(
-        "Create a set of potential interview questions and talking "
-        "points based on the tailored resume and job requirements. "
-        "Utilize tools to generate relevant questions and discussion "
-        "points. Make sure to use these question and talking points to "
-        "help the candiadte highlight the main points of the resume "
-        "and how it matches the job posting."
+        """Create a set of potential interview questions and talking
+        points based on the tailored resume and job requirements.
+        Utilize tools to generate relevant questions and discussion
+        points. Make sure to use these question and talking points to
+        help the candiadte highlight the main points of the resume
+        and how it matches the job posting."""
     ),
     expected_output=(
-        "A document containing key questions and talking points "
-        "that the candidate should prepare for the initial interview."
+        """A document containing key questions and talking points
+        that the candidate should prepare for the initial interview."""
     ),
     output_file="interview_materials.md",
     context=[research_task, profile_task, resume_strategy_task],
     agent=interview_preparer
 )
 
-# Task for Markdown to PDF Converter Agent: Convert Tailored Resume to PDF
-markdown_to_pdf_task = Task(
-    description=(
-        """Convert the tailored resume from markdown format to PDF format."""
-    ),
-    expected_output=(
-        """A PDF version of the tailored resume. Only the resume should be converted to PDF,
-        not any part of the conversation or other information."""
-    ),
-    output_file="tailored_resume.pdf",
-    context=[resume_strategy_task],
-    agent=markdown_to_pdf_converter
-)
-
 job_application_crew = Crew(
     agents=[researcher,
             profiler,
             resume_strategist,
-            interview_preparer,
-            markdown_to_pdf_converter],
+            interview_preparer],
 
     tasks=[research_task,
            profile_task,
            resume_strategy_task,
-           interview_preparation_task,
-           markdown_to_pdf_task],
+           interview_preparation_task],
 
-    verbose=True
+    verbose=True,
+    memory=True,
+    cache=True
 )
 
 job_application_inputs = {
@@ -207,7 +182,7 @@ job_application_inputs = {
     'personal_writeup': """Joshua is an experienced generalist software engineer.
     He started his career as a backend Java developer, but has since transitioned
     to DevOps and Site Reliability Engineering. He has a strong interest in automation,
-    Kubernetes, platform engineering, and cloud technologies. Joshua is passionate 
+    Kubernetes, platform engineering, and cloud technologies. Joshua is passionate
     about learning to apply AI to solve real-world problems and is currently exploring
     AI agents with Python and CrewAI. He is seeking a role that allows him to leverage
     his diverse skill set and contribute to innovative projects.
@@ -233,18 +208,9 @@ def prepare_resume(job_posting_url, github_url, personal_writeup, output_dir):
         'personal_writeup': personal_writeup
     }
 
-    result = job_application_crew.kickoff(inputs=job_application_inputs)
-
-    if result:
-        print("Job application process completed successfully.")
-        print("Tailored resume saved as PDF.")
-    else:
-        print("Job application process failed. Please check the inputs and try again.")
-
-    # Move the PDF file to the specified output directory
-    if result and output_dir:
-        pdf_file_path = os.path.join('./tailored_resume.pdf')
-        shutil.move(pdf_file_path, output_dir)
+    job_application_crew.kickoff(inputs=job_application_inputs)
+    tool = MarkdownToPDFTool(markdown_file_path="tailored_resume.md")
+    tool._run()
 
 
 if __name__ == '__main__':
